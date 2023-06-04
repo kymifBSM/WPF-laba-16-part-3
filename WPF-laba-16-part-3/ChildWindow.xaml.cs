b66ecc3c8f98d62b593c8ed2d512eeb427c26e23
kymifBSM <sandmanfin@gmail.com>
2023-06-04 23:44:58 +0700

    task3

---
+++ WPF-laba-16-part-3/ChildWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_laba_16_part_3
+{
+    /// <summary>
+    /// Логика взаимодействия для ChildWindow.xaml
+    /// </summary>
+    public partial class ChildWindow : Window
+    {
+        public string Word { get; set; }
+
+        public ChildWindow(string word)
+        {
+            InitializeComponent();
+            Word = word;
+            DataContext = this;
+        }
+    }
+}
---
+++ WPF-laba-16-part-3/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_laba_16_part_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : NavigationWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SplitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string inputText = InputTextBox.Text;
+            string[] words = inputText.Split(' ');
+
+            foreach (string word in words)
+            {
+                ChildWindow childWindow = new ChildWindow(word);
+                childWindow.Show();
+            }
+        }
+    }
+}
